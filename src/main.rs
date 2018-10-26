a9c68379186ebbf5312c2d5e1b529e94f6fce056
phonnz <alfonso@500startups.com>
2018-10-26 18:42:38 -0500

    implement cd command

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,61 @@
 use std::io;
 use std::io::prelude::*;
+use std::fs;
+use std::process::Command;
+use std::env;
+use std::path::Path;
 
-fn parse_cmd(arg){
-    String::split(arg, "");
+fn  change_directory(path: &str){
+    let current_path = Path::new(path);
+    env::set_current_dir(&current_path);
+    
 }
 
-fn run_cmd(arg: String){
-    let args = parse_cmd(arg);
-    println!("{:?}", args);
+fn list_items(){
+    let paths = fs::read_dir("/home/phonnz/").unwrap();
+
+    for path in paths {
+            println!("{:?}", path);
+    }
+}
+
+fn parse_cmd(cmd: &String) -> Vec<&str> {
+    let args = cmd.trim().split_whitespace();
+     args.collect::<Vec<&str>>()
+    
+}
+
+fn run_cmd(cmd: &String){
+    let mut args = parse_cmd(cmd);
+    match args[0] {
+        // "ls" => {
+        //     list_items();
+        // },
+        "cd" => {
+            if args.len() > 1 {
+                change_directory(args[1]);
+            }else{
+                println!("Error: command cd requires a path");
+            }
+        },
+        _ => {
+            let mut list_dir = Command::new(args[0]);
+            if args.len() > 1 {
+                args.reverse();
+                args.pop();
+                for a in args{
+                    list_dir.arg(a);
+                }
+            }
+            list_dir.status().expect("failed to execute process");
+            // println!("");
+            // list_dir.current_dir("/");
+            // list_dir.status().expect("failed to execute process");
+            
+        }
+        // ,
+        // _ => println!("command not found: {}", args[0])
+    }
 }
 
 
@@ -15,7 +63,13 @@ fn run_cmd(arg: String){
 fn main() {
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
-        run_cmd(line.unwrap());
-        // println!("{}", line.unwrap());
+        run_cmd(&line.unwrap());
+        // if "" == line.unwrap().clone() {
+        //    println!(""); 
+        // }else{
+        //     run_cmd(&line.unwrap());
+
+        // }
+        
     }
 }
